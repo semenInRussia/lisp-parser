963a1c8d68ce540bcc50dbf9e02676121a140c4c
semenInRussia <semenprofis999@gmail.com>
2021-04-06 22:21:39 +0300

    add easy CLI interface

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 
-#include "math.h"
 #include "integers.h"
 
 #define DEBUG 1
@@ -25,7 +26,7 @@
 
 #define DIVISION_CHAR '/'
 
-typedef int (*operation_t) (const int[MAX_ELEMENTS_FOR_COMPUTE], uint32_t);
+typedef int (*operation_t)(const int[MAX_ELEMENTS_FOR_COMPUTE], uint32_t);
 
 
 void skipSymbols(char **currentChar, int amountSymbols) {
@@ -68,7 +69,7 @@ void parseIntAndAddToArray(char **currentChar, int *pointerOnArray, int *inUseEl
     skipSymbols(currentChar, lengthOfInteger);
 }
 
-int sum(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+int iSum(const int *numbersForCompute, uint32_t length) {
     int result = 0;
 
     for (uint32_t i = 0; i < length; i++) {
@@ -78,14 +79,14 @@ int sum(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) 
     return result;
 }
 
-int sub(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+int iSub(const int *numbersForCompute, uint32_t length) {
     int decreasing = numbersForCompute[0];
-    int subtraction = sum(&(numbersForCompute[1]), length - 1);
+    int subtraction = iSum(&(numbersForCompute[1]), length - 1);
 
     return decreasing - subtraction;
 }
 
-int mult(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+int iMult(const int *numbersForCompute, uint32_t length) {
     int result = 1;
 
     for (uint32_t i = 0; i < length; i++) {
@@ -95,9 +96,9 @@ int mult(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length)
     return result;
 }
 
-int div(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+int iDiv(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
     int divisor = numbersForCompute[0];
-    int divisible = sum(&(numbersForCompute[1]), length);
+    int divisible = iSum(&(numbersForCompute[1]), length);
 
     return divisor / divisible;
 }
@@ -109,16 +110,16 @@ void handleOperation(
 
     switch (**currentChar) {
         case PLUS_CHAR:
-            *pointerOnOperation = &sum;
+            *pointerOnOperation = &iSum;
             break;
         case SUB_CHAR:
-            *pointerOnOperation = &sub;
+            *pointerOnOperation = &iSub;
             break;
         case MULTIPLICATION_CHAR:
-            *pointerOnOperation = &mult;
+            *pointerOnOperation = &iMult;
             break;
         case DIVISION_CHAR:
-            *pointerOnOperation = &div;
+            *pointerOnOperation = &iDiv;
             break;
     }
 
@@ -210,47 +211,15 @@ int lispToInt(const char *lispExpression) {
 }
 
 
-int main() {
-    int powerRes = iPow(10, 2);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        perror("You're not enter a LISP expression!!!");
+        exit(EINVAL);
+    }
 
-#if DEBUG
-    printf("power result [ %d ]\n", powerRes);
-#endif
+    char* lispExpression = argv[1];
 
-    assert(powerRes == 100);
-
-    int parsedInt = parseInt("1001 ");
-    int parsedIntByLength = parseStrIntByLength("1001 ", 4);
-    uint32_t lengthOfInteger = getLengthOfStrInt("1001 ");
-
-#if DEBUG
-    printf("parsed integer is [ %d ]\n", parsedInt);
-    printf("length of integer is [ %d ]\n", lengthOfInteger);
-    printf("parsed integer by length is [ %d ]\n", parsedIntByLength);
-#endif
-
-    assert(parsedInt == 1001);
-    assert(lengthOfInteger == 4);
-    assert(parsedIntByLength == 1001);
-
-    int negativeParsedInt = parseInt("-100 ");
-    int lengthOfNegativeParsedInt = getLengthOfStrInt("-100 ");
-
-#if DEBUG
-    printf("Negative parsed int is: [ %d ]\n", negativeParsedInt);
-    printf("length of Negative parsed int is: [ %d ]\n", lengthOfNegativeParsedInt);
-#endif
-
-    assert(lengthOfNegativeParsedInt == 4);
-    assert(negativeParsedInt == -100);
-
-    int lispResult = lispToInt("(* 3 2 1)");
-
-#if DEBUG
-    printf("result of LISP expression is [ %d ]\n", lispResult);
-#endif
-
-    assert(lispResult == 6);
+    printf("Result is:\n %d", lispToInt(lispExpression));
 
     return 0;
 }
