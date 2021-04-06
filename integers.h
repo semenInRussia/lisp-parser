d0f0bf5f5b03179ff254998656e8060e1bf1c9d8
semenInRussia <semenprofis999@gmail.com>
2021-04-06 22:14:09 +0300

    Add able to compute with difference arithmetic operation

--- integers.h
+++ integers.h
@@ -5,6 +5,6 @@
 
 int parseInt(char *string);
 
-unsigned int getLengthOfStrInt(char *string);
+int getLengthOfStrInt(char *string);
 
 int parseStrIntByLength(char *strInt, unsigned int length);
--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define DEBUG 1
 #define MAX_ELEMENTS_FOR_COMPUTE 10
 
-#define NEED_ADD_LISP_EXPRESSION_TO_RESULT_AND_INC_CHAR 2
+#define NEED_ADD_LISP_EXPRESSION_TO_RESULT_AND_SKIP_TO_CLOSING_BRACKET 1
 
 #define ASCII_CODE(ch) (uint8_t) ch
 #define ASCII_CODE_ZERO ASCII_CODE('0')
@@ -16,6 +16,17 @@
 #define ASCII_CODE_NINE ASCII_CODE('9')
 
 #define IS_CHAR_NUMBER(ch)  ASCII_CODE(ch) >= ASCII_CODE_ZERO  && ASCII_CODE(ch) <= ASCII_CODE_NINE
+
+#define PLUS_CHAR '+'
+
+#define SUB_CHAR '-'
+
+#define MULTIPLICATION_CHAR '*'
+
+#define DIVISION_CHAR '/'
+
+typedef int (*operation_t) (const int[MAX_ELEMENTS_FOR_COMPUTE], uint32_t);
+
 
 void skipSymbols(char **currentChar, int amountSymbols) {
     (*currentChar) += amountSymbols;
@@ -27,16 +38,104 @@ void skipSpaces(char **currentChar) {
     }
 }
 
+int getAmountOfSymbolsFromCurrentSymbolToClosingBracket(char *currentSymbol) {
+    int amountOfSymbols = 0;
+
+    for (; *currentSymbol != ')'; currentSymbol++) {
+        amountOfSymbols++;
+    }
+
+    return amountOfSymbols;
+}
+
+void skipSymbolsToClosingBracket(char **currentChar) {
+    skipSymbols(
+            currentChar,
+            getAmountOfSymbolsFromCurrentSymbolToClosingBracket(*currentChar)
+    );
+}
+
+void parseIntAndAddToArray(char **currentChar, int *pointerOnArray, int *inUseElementsOfArray) {
+    int lengthOfInteger = getLengthOfStrInt(*currentChar);
+
+    *(pointerOnArray + (*inUseElementsOfArray)) = parseStrIntByLength(
+            *currentChar,
+            lengthOfInteger
+    );
+
+    (*inUseElementsOfArray)++;
+
+    skipSymbols(currentChar, lengthOfInteger);
+}
+
+int sum(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+    int result = 0;
+
+    for (uint32_t i = 0; i < length; i++) {
+        result += numbersForCompute[i];
+    }
+
+    return result;
+}
+
+int sub(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+    int decreasing = numbersForCompute[0];
+    int subtraction = sum(&(numbersForCompute[1]), length - 1);
+
+    return decreasing - subtraction;
+}
+
+int mult(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+    int result = 1;
+
+    for (uint32_t i = 0; i < length; i++) {
+        result *= numbersForCompute[i];
+    }
+
+    return result;
+}
+
+int div(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
+    int divisor = numbersForCompute[0];
+    int divisible = sum(&(numbersForCompute[1]), length);
+
+    return divisor / divisible;
+}
+
+void handleOperation(
+        char **currentChar,
+        operation_t *pointerOnOperation
+) {
+
+    switch (**currentChar) {
+        case PLUS_CHAR:
+            *pointerOnOperation = &sum;
+            break;
+        case SUB_CHAR:
+            *pointerOnOperation = &sub;
+            break;
+        case MULTIPLICATION_CHAR:
+            *pointerOnOperation = &mult;
+            break;
+        case DIVISION_CHAR:
+            *pointerOnOperation = &div;
+            break;
+    }
+
+    skipSymbols(currentChar, 1);
+}
+
 int handleSymbol(
         char **currentChar,
 
         int *numbersForCompute,
         int *inUseNumbersForCompute,
 
+        operation_t *pointerOnOperation,
+
         uint32_t *openingBrackets,
         uint32_t *closingBrackets
 ) {
-
     switch (**currentChar) {
         case ' ':
             skipSpaces(currentChar);
@@ -49,48 +148,24 @@ int handleSymbol(
             (*openingBrackets)++;
 
             if (*openingBrackets > 1) {
-                return NEED_ADD_LISP_EXPRESSION_TO_RESULT_AND_INC_CHAR;
+                return NEED_ADD_LISP_EXPRESSION_TO_RESULT_AND_SKIP_TO_CLOSING_BRACKET;
             } else {
-                skipSymbols(currentChar, 1);
+                skipSymbols(currentChar, 1); // Skip 1 Open Bracket: '('
+                skipSpaces(currentChar);
+                handleOperation(currentChar, pointerOnOperation);
             }
             break;
         default:
             if (IS_CHAR_NUMBER(**currentChar) || **currentChar == '-') {
-                uint32_t lengthOfInteger = getLengthOfStrInt(*currentChar);
-                *(numbersForCompute + (*inUseNumbersForCompute)) = parseStrIntByLength(
-                        *currentChar,
-                        lengthOfInteger
-                );
-                (*inUseNumbersForCompute)++;
-
-                skipSymbols(currentChar, (int) (lengthOfInteger));
+                parseIntAndAddToArray(currentChar, numbersForCompute, inUseNumbersForCompute);
             } else {
+                // Undefined symbol...
                 skipSymbols(currentChar, 1);
             }
+            break;
     }
 
     return 0;
-}
-
-
-int getAmountOfSymbolsFromCurrentSymbolToClosingBracket(char *currentSymbol) {
-    int amountOfSymbols = 0;
-
-    for (; *currentSymbol != ')'; currentSymbol++) {
-        amountOfSymbols++;
-    }
-
-    return amountOfSymbols;
-}
-
-int sum(const int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE], uint32_t length) {
-    int result = 0;
-
-    for (uint32_t i = 0; i < length; i++) {
-        result += numbersForCompute[i];
-    }
-
-    return result;
 }
 
 int lispToInt(const char *lispExpression) {
@@ -106,17 +181,22 @@ int lispToInt(const char *lispExpression) {
     int numbersForCompute[MAX_ELEMENTS_FOR_COMPUTE] = {0};
     int inUseNumbersForCompute = 0;
 
+    int (*operation)(const int *, uint32_t);
+
     char *current_symbol = (char *) lispExpression;
 
     while (((openingBrackets != closingBrackets) || (openingBrackets == 0)) && (*current_symbol != '$')) {
         if (handleSymbol(
                 &current_symbol,
                 numbersForCompute, &inUseNumbersForCompute,
+                &operation,
                 &openingBrackets, &closingBrackets
-        ) == NEED_ADD_LISP_EXPRESSION_TO_RESULT_AND_INC_CHAR) {
-            *(numbersForCompute + inUseNumbersForCompute) = lispToInt(current_symbol);
+        ) == NEED_ADD_LISP_EXPRESSION_TO_RESULT_AND_SKIP_TO_CLOSING_BRACKET) {
+
+            numbersForCompute[inUseNumbersForCompute] = lispToInt(current_symbol);
             inUseNumbersForCompute++;
-            current_symbol += getAmountOfSymbolsFromCurrentSymbolToClosingBracket(current_symbol);
+            skipSymbolsToClosingBracket(&current_symbol);
+
         }
     }
 
@@ -126,7 +206,7 @@ int lispToInt(const char *lispExpression) {
 
     assert(openingBrackets == closingBrackets);
 
-    return sum(numbersForCompute, (uint32_t) inUseNumbersForCompute);
+    return (*operation)(numbersForCompute, (uint32_t) inUseNumbersForCompute);
 }
 
 
@@ -164,13 +244,13 @@ int main() {
     assert(lengthOfNegativeParsedInt == 4);
     assert(negativeParsedInt == -100);
 
-    int lispResult = lispToInt("(+ -100 50 (+ 10 101 109 -10))");
+    int lispResult = lispToInt("(* 3 2 1)");
 
 #if DEBUG
     printf("result of LISP expression is [ %d ]\n", lispResult);
 #endif
 
-    assert(lispResult == 160);
+    assert(lispResult == 6);
 
     return 0;
 }
